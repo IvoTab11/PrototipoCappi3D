a1e5d872168742633aebed37af2c966c2df984fa
IvoTab11 <ivoagusmitasil@gmail.com>
2024-09-30 00:49:25 -0300

    Se incluyeron 3 nuevos tipos de contenedores. Los comandos de voz funcionan

--- PrototipoCappi/Assets/Script/DejarBasura.cs
+++ PrototipoCappi/Assets/Script/DejarBasura.cs
@@ -9,10 +9,11 @@ public class DejarBasura : MonoBehaviour
    public GameObject contenedor;
    public GameObject dejarIndicator; // Objeto visual que representa el área de recogida
 
-    //private bool canPickup = false;
-    //private bool dejar = false;
-    private bool puedeReciclar = false;
-    private bool puedeDesechar = false;
+    // private bool puedeReciclar = false;
+    // private bool puedeDesechar = false;
+    private bool tirarCarton = false;
+    private bool tirarPlastico = false;
+    private bool tirarVidrio = false;
 
      // Referencia al script RecogerBasura
     private RecogerBasura recogerBasura;
@@ -28,10 +29,12 @@ public class DejarBasura : MonoBehaviour
         // Encontrar el script RecogerBasura
         recogerBasura = FindObjectOfType<RecogerBasura>();
         
-       //keywords.Add("recoger", () => { RecogerBasuraCommand(); });
-       //keywords.Add("dejar", () => { DejarBasuraCommand(); });
-       keywords.Add("reciclar", () => { ReciclarCommand(); });
-       keywords.Add("desechar", () => { DesecharCommand(); });
+    //    keywords.Add("reciclar", () => { ReciclarCommand(); });
+    //    keywords.Add("desechar", () => { DesecharCommand(); });
+       keywords.Add("carton", () => { CartonCommand(); });
+       keywords.Add("plastico", () => { PlasticoCommand(); });
+       keywords.Add("vidrio", () => { VidrioCommand(); });
+
         // Iniciar el reconocimiento de voz
         keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized +=  keywordRecognizer_OnPhraseRecognized;
@@ -47,67 +50,87 @@ public class DejarBasura : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Reciclable") || other.CompareTag("Desechable")) 
-        {
-            //dejar = true;
-            puedeDesechar = true;
-            puedeReciclar = true;
-            Debug.Log("Camión dentro del área de descarga."); // Debug para verificar
-        }
-        // if(other.CompareTag("ContenedorGrande"))
+        // if (other.CompareTag("Carton")) 
         // {
-        //     dejar = true;
-        //     Debug.Log("Camión dentro del área de descarga.");
+        //     tirarCarton = true;
+        //    // Debug.Log("Camión dentro del área de descarga."); // Debug para verificar
+        // }else if(other.CompareTag("Plastico")){
+        //     tirarPlastico = true;
+        // }else{
+        //     tirarVidrio= true;
         // }
+        if (other.CompareTag("Carton")) 
+        {
+            tirarCarton = true;
+            tirarPlastico = false;
+            tirarVidrio = false;
+        }
+        else if(other.CompareTag("Plastico"))
+        {
+            tirarPlastico = true;
+            tirarCarton = false;
+            tirarVidrio = false;
+        }
+        else if(other.CompareTag("Vidrio"))
+        {
+            tirarVidrio = true;
+            tirarCarton = false;
+            tirarPlastico = false;
+        }
+        
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Reciclable") || other.CompareTag("Desechable"))
-        {
-            //dejar = false;
-            puedeDesechar = false;
-            puedeReciclar = false;
-            Debug.Log("Camión fuera del área de descarga."); // Debug para verificar
-        }
-        // if (other.CompareTag("ContenedorGrande"))
+        // if (other.CompareTag("Carton"))
         // {
-        //     dejar = false;
-        //     Debug.Log("Camión fuera del área de descarga."); // Debug para verificar
+        //     //dejar = false;
+        //     // puedeDesechar = false;
+        //     // puedeReciclar = false;
+        //     tirarCarton = false;
+        //     //Debug.Log("Camión fuera del área de descarga."); // Debug para verificar
+        // }else if(other.CompareTag("Plastico")){
+        //     tirarPlastico = false;
+        // }else{
+        //     tirarVidrio= false;
         // }
+        if (other.CompareTag("Carton"))
+        {
+            tirarCarton = false;
+        }
+        else if(other.CompareTag("Plastico"))
+        {
+            tirarPlastico = false;
+        }
+        else if(other.CompareTag("Vidrio"))
+        {
+            tirarVidrio = false;
+        }
     }
 
-    // void DejarBasuraCommand()
-    // {
-    //     if(dejar)
-    //     {
-    //         Debug.Log("Comando de voz detectado: dejar");
-    //         // reciclable-=1;
-    //         // Debug.Log("Basura reciclable: "+ reciclable);
-    //         // desechable-=1;
-    //         // Debug.Log("Basura desechable: "+ desechable);
-    //         recogerBasura.reciclable-=1;
-    //         Debug.Log("Basura reciclable: "+ recogerBasura.reciclable);
-    //         recogerBasura.desechable-=1;
-    //         Debug.Log("Basura desechable: "+ recogerBasura.desechable);
-    //     }
-    // }
-    void ReciclarCommand()
-    {
-        if(puedeReciclar && recogerBasura.reciclable!=0){
-            Debug.Log("Comando de voz detectado: reciclar");
-            recogerBasura.reciclable-=1;
-            ScriptGameManager.instance.SumarPuntosR(valorSuma);
-            Debug.Log("Basura reciclable: "+ recogerBasura.reciclable);
+    
+    void CartonCommand(){
+        if(tirarCarton && recogerBasura.carton!=0){
+            Debug.Log("Comando de voz detectado: carton");
+            recogerBasura.carton-=recogerBasura.carton;
+            ScriptGameManager.instance.SumarPuntosD(valorSuma);
+            Debug.Log("Carton: "+ recogerBasura.carton);
         }
     }
-    void DesecharCommand()
-    {
-        if(puedeDesechar && recogerBasura.desechable!=0){
-            Debug.Log("Comando de voz detectado: desechar");
-            recogerBasura.desechable-=1;
+    void PlasticoCommand(){
+        if(tirarPlastico && recogerBasura.plastico!=0){
+            Debug.Log("Comando de voz detectado: plastico");
+            recogerBasura.plastico-=recogerBasura.plastico;
             ScriptGameManager.instance.SumarPuntosD(valorSuma);
-            Debug.Log("Basura desechable: "+ recogerBasura.desechable);
+            Debug.Log("Plastico: "+ recogerBasura.plastico);
+        }
+    }
+    void VidrioCommand(){
+        if(tirarVidrio && recogerBasura.vidrio!=0){
+            Debug.Log("Comando de voz detectado: vidrio");
+            recogerBasura.vidrio-=recogerBasura.vidrio;
+            ScriptGameManager.instance.SumarPuntosD(valorSuma);
+            Debug.Log("Vidrio: "+ recogerBasura.vidrio);
         }
     }
 
