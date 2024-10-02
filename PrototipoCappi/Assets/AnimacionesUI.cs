533a81aa955a4e358a187b417d1dd2a7674318a0
celidearaceli <celidearaceli@gmail.com>
2024-10-02 13:57:30 -0300

    MAIN MENU TERMINANDO SI QUERES AGREGALE ALGO, HACELO :D

--- PrototipoCappi/Assets/AnimacionesUI.cs
+++ PrototipoCappi/Assets/AnimacionesUI.cs
@@ -6,7 +6,16 @@ public class AnimacionesUI : MonoBehaviour
 {
     [SerializeField] private GameObject logo;
 
+    [SerializeField] private GameObject inicioGrupo;
+
+
     private void Start(){
-        LeanTween.moveX(logo.GetComponent<RectTransform>(), 170, 1.5f).setDelay(2.5f).setEase(LeanTweenType.easeOutBounce);
+        LeanTween.moveX(logo.GetComponent<RectTransform>(), 0, 1.5f).setDelay(1f).setEase(LeanTweenType.easeOutBounce).setOnComplete(BajarAlpha);
+    }
+
+    private void BajarAlpha(){
+
+        LeanTween.alpha(inicioGrupo.GetComponent<RectTransform>(), 0f, 1f).setDelay(0.5f);
+        inicioGrupo.GetComponent<CanvasGroup>().blocksRaycasts= false;
     }
 }
